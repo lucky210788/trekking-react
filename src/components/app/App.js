4a785581b3c24704b64371977ab50f3dd7fc1e43
Igor Patynok <lucky210788@mail.ru>
2019-12-01 13:41:09 +0200

    complite

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
+import scrollToComponent from 'react-scroll-to-component';
 
-function App() {
-  return (
-    <div className="App">
-     yoyo
-    </div>
-  );
+import WelcomeSection from '../welcomeSection/WelcomeSection';
+import Content from '../content/Content';
+import AboutUs from '../aboutUs/AboutUs';
+import Article from '../article/Article';
+import Footer from '../footer/Footer';
+import './fonts.scss';
+import './reset.scss';
+
+export default class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            content: null
+        }
+    }
+
+    findContent = (content) => {
+        this.setState({content});
+        console.log(this.state.content);
+    };
+
+    render() {
+        console.log(this.state.content);
+        return (
+            <Fragment>
+                <WelcomeSection scrollTo={() => scrollToComponent(this.state.content, { offset: 0, align: 'top', duration: 1500})}/>
+                <Content findContent={this.findContent}/>
+                <AboutUs/>
+                <Article/>
+                <Footer/>
+            </Fragment>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './footer.scss';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="left-block">
+                    <p className="footer-title">Trekking</p>
+                    <p className="footer-main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div className="right-block">
+                    <ul className="footer-nav">
+                        <li><a href="#">credits</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">about</a></li>
+                        <li><a href="#">contact</a></li>
+                    </ul>
+                    <p className="author">Design by gianluca cosetta</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
